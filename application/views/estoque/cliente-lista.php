86294988c315fbb150afacd156adb754b65748fd
sisprod <sisprod@sisprod-GA-VM900M>
2016-10-20 15:04:00 -0300

    johnny cod 606 (listar tabelas em ordem alfabetica)

--- application/views/estoque/cliente-lista.php
+++ application/views/estoque/cliente-lista.php
@@ -35,7 +35,7 @@
                 ?>
                 <tbody>
                     <?php
-                        $lista = $this->cliente->listar($_GET)->limit($limit, $pagina)->get()->result();
+                        $lista = $this->cliente->listar($_GET)->orderby('nome')->limit($limit, $pagina)->get()->result();
                         $estilo_linha = "tabela_content01";
                         foreach ($lista as $item) {
                             ($estilo_linha == "tabela_content01") ? $estilo_linha = "tabela_content02" : $estilo_linha = "tabela_content01";
