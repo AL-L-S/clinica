6ff7ec8b1a72df9ab66bf8c3cc48239de44834fc
sisprod <sisprod@sisprod-Inspiron-N5110>
2017-02-23 15:32:13 -0300

    Cleyson

--- application/models/cadastro/contasreceber_model.php
+++ application/models/cadastro/contasreceber_model.php
@@ -23,6 +23,16 @@ class contasreceber_model extends Model {
     }
 
     function listar($args = array()) {
+        if (isset($args['nome']) && strlen($args['nome']) > 0) {
+            $this->db->select('
+                            tes.descricao
+                            ');
+            $this->db->from('tb_tipo_entradas_saida tes');
+            $this->db->where('tes.ativo', 'true');
+            $this->db->where('tes.tipo_entradas_saida_id', $args['nome']);
+            $return = $this->db->get()->result();
+        }
+        
         $this->db->select('fc.financeiro_contasreceber_id,
                             fc.valor,
                             fc.devedor,
@@ -39,12 +49,12 @@ class contasreceber_model extends Model {
         $this->db->where('fc.ativo', 'true');
         $this->db->join('tb_forma_entradas_saida fe', 'fe.forma_entradas_saida_id = fc.conta', 'left');
         $this->db->join('tb_financeiro_credor_devedor cd', 'cd.financeiro_credor_devedor_id = fc.devedor', 'left');
-        $this->db->join('tb_financeiro_classe f', 'f.descricao = fc.classe', 'left');
+//        $this->db->join('tb_financeiro_classe f', 'f.descricao = fc.classe', 'left');
         if (isset($args['empresa']) && strlen($args['empresa']) > 0) {
             $this->db->where('fc.devedor', $args['empresa']);
         }
         if (isset($args['nome']) && strlen($args['nome']) > 0) {
-            $this->db->where('tipo_id', $args['nome']);
+            $this->db->where('tipo', $return[0]->descricao);
         }
         if (isset($args['nome_classe']) && strlen($args['nome_classe']) > 0) {
             $this->db->where('fc.classe', $args['nome_classe']);
