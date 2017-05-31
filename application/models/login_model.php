442468430dcf7469177be3f0a050e9a503c632e0
sisprod <sisprod@sisprod-Johnny>
2017-05-31 18:30:55 -0300

    Johnny

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -141,7 +141,7 @@ class login_model extends Model {
                 $this->db->set('paciente_id', $item->paciente_id);
                 $this->db->set('empresa_id', $empresa_id);
                 $this->db->set( 'numero', preg_replace('/[^\d]+/', '', $item->celular) );
-                $this->db->set('mensagem', $mensagem . " Exame: " . $item->nome);
+                $this->db->set('mensagem', $mensagem . " Consulta: " . $item->nome);
                 $this->db->set('tipo', 'CONFIRMACAO');
                 $this->db->set('data', $horario);
                 $this->db->insert('tb_sms');
