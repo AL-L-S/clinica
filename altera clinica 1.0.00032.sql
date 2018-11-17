001b5035c69a975c821e35bdebda9ff0b2094364
sisprod <stgsaude@gmail.com>
2018-11-17 09:37:45 -0300

    Iniciando 33

--- altera clinica 1.0.00032.sql
+++ altera clinica 1.0.00032.sql
@@ -210,3 +210,19 @@ END;
 $$ LANGUAGE plpgsql;
 SELECT insereValor();
 
+-- Fechando a versao  17/11
+
+CREATE OR REPLACE FUNCTION insereValor()
+RETURNS text AS $$
+DECLARE
+    resultado integer;
+BEGIN
+    resultado := ( SELECT COUNT(*) FROM ponto.tb_versao WHERE sistema = '1.0.000032');
+    IF resultado = 0 THEN 
+	INSERT INTO ponto.tb_versao(sistema, banco_de_dados)
+        VALUES ('1.0.000032', '1.0.000032');
+    END IF;
+    RETURN 'SUCESSO';
+END;
+$$ LANGUAGE plpgsql;
+SELECT insereValor();
